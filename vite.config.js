0e8ed08bb62f16be19769c91f6b35637274c0b3b
PREM015 <rprem3096@gamil.com>
2025-04-06 22:15:36 +0530

    push 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 Add this line to fix blank screen on Netlify
   plugins: [
-    tailwindcss(),react()],
+    tailwindcss(),
+    react()
+  ],
 })
