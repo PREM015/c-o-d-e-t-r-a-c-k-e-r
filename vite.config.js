e3be9744c4e9d7babfafe3f7d2a10059ee19e073
PREM015 <152625984+PREM015@users.noreply.github.com>
2025-05-31 13:41:26 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative paths for assets
+
   plugins: [
-    require("tailwindcss-animate"),
     tailwindcss(),
     react()
   ],
-})
+});
